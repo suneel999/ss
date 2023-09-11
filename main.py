04e46eeb5ffa2a3f0e4120dfdd841b815d01ff48
suneel999 <suneelraju999@gmail.com>
2023-09-11 12:55:59 +0530

    Add main.py script

---
+++ main.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+
+product_names = []
+product_prices = []
+product_descriptions = []
+
+search_query = input("What mobile do you want to search? ")
+page_number = 1
+
+while True:
+    url = f"https://www.flipkart.com/search?q={search_query}&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off&page={page_number}"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    # Find all the product names on the page
+    names = soup.find_all("div", class_="_4rR01T")
+    prices = soup.find_all("div", class_="_30jeq3 _1_WHN1")
+    descriptions = soup.find_all("div", class_="fMghEO")
+
+    # If no more products are found on the page, break the loop
+    if not names:
+        break
+
+    # Extract and append the text of each product name, price, and description to respective lists
+    for name, price, desc in zip(names, prices, descriptions):
+        product_names.append(name.text)
+        product_prices.append(price.text)
+        product_descriptions.append(desc.text)
+
+    # Move to the next page
+    page_number += 1
+
+# Print the collected data
+for i, (name, price, desc) in enumerate(zip(product_names, product_prices, product_descriptions), start=1):
+    print(f"{i}. Name: {name}")
+    print(f"   Price: {price}")
+    print(f"   Description: {desc}\n")
